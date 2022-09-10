fe5b998fd630b3dff14ba79f490601b03bebca2e
Norayr <norayr@tests-MacBook-Pro.local>
2022-09-10 15:52:07 +0400

    remove smart pointers project and add it to basics project

--- Smart Pointers/Smart Pointers/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "UnitTests.h"
-
-int main()
-{
-	Testing::TestingUniquePointer();
-	Testing::TestingSharedPointer();
-	return 0;
-}
--- Smart Pointers/Smart Pointers/UnitTests.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-namespace Testing
-{
-	void TestingUniquePointer();
-	void TestingSharedPointer();
-}
